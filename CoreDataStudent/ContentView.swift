3b640d7eacebf1d3b5d4b25c234bb55106a10a1c
Olivier Van hamme <nicefiction@gmail.com>
2021-07-28 20:46:48 +0200

    Completes Day 53.4 : How to combine Core Data and SwiftUI .

--- CoreDataStudent/ContentView.swift
+++ CoreDataStudent/ContentView.swift
@@ -1,80 +1,62 @@
-//
-//  ContentView.swift
-//  CoreDataStudent
-//
-//  Created by Olivier Van hamme on 28/07/2021.
-//
+// ContentView.swift
+// SOURCE: https://www.hackingwithswift.com/books/ios-swiftui/how-to-combine-core-data-and-swiftui
 
 import SwiftUI
 import CoreData
 
+
+
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+   
+   // MARK: - PROPERTY WRAPPERS
+   // @Environment(\.managedObjectContext) private var viewContext
+   @Environment(\.managedObjectContext) var moc
+   /// Retrieving information from Core Data is done using a fetch request :
+   @FetchRequest(entity: Student.entity(),
+                 sortDescriptors: []) var students: FetchedResults<Student>
+   
+   
+   
+   // MARK: - COMPUTED PROPERTIES -
+   
+   var body: some View {
+      
+      NavigationView {
+         List {
+            ForEach(students) { (student: Student) in
+               Text(student.name ?? "N/A")
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+         }
+         Button("Create Students") {
+            let firstnames: [String] = [ "Dorothy", "Ozma", "Glinda", "Olivia" ]
+            let lastnames: [String] = [ "Gale", "Yellow", "Magenta", "Blue" ]
+            
+            let randomFirstname: String = firstnames.randomElement() ?? "N/A"
+            let randomLastname: String = lastnames.randomElement() ?? "N/A"
+            
+            let newStudent: Student = Student(context: self.moc)
+            newStudent.id = UUID()
+            newStudent.name = "\(randomFirstname) \(randomLastname)"
+            try? self.moc.save()
+         }
+         .padding()
+         .navigationBarTitle(Text("CoreData Students"))
+      }
+   }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
-}
+
+
+
+// MARK: - PREVIEWS -
+
+//struct ContentView_Previews: PreviewProvider {
+//
+//   static var previews: some View {
+//
+//      ContentView()
+//      ContentView().environment(\.managedObjectContext,
+//                                PersistenceController.preview.container.viewContext)
+//   }
+//}
--- CoreDataStudent/CoreDataStudentApp.swift
+++ CoreDataStudent/CoreDataStudentApp.swift
@@ -1,20 +1,32 @@
-//
-//  CoreDataStudentApp.swift
-//  CoreDataStudent
-//
-//  Created by Olivier Van hamme on 28/07/2021.
-//
+// CoreDataStudentApp.swift
+
+/// Setting up Core Data requires two steps :
+/// 1. Creating what is called a `persistent container` ,
+/// which is what loads and saves the actual data from device storage ,
+/// and 2. Injecting that into the SwiftUI `environment`
+/// so that all our views can access it .
 
 import SwiftUI
 
+
+
 @main
 struct CoreDataStudentApp: App {
-    let persistenceController = PersistenceController.shared
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
-    }
+   
+   // MARK: - PROPERTIES
+   
+   let persistenceController = PersistenceController.shared
+   
+   
+   
+   // MARK: - COMPUTED PROPERTIES
+   
+   var body: some Scene {
+      
+      WindowGroup {
+         ContentView()
+            .environment(\.managedObjectContext,
+                         persistenceController.container.viewContext)
+      }
+   }
 }
